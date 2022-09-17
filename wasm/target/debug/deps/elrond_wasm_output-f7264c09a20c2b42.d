1dc41ff96674db54b0daa522f43386271e94d2b0
René Ďuriš <88586343+ReneDuris@users.noreply.github.com>
2022-09-18 01:48:05 +0200

    Delete wasm/target directory

--- wasm/target/debug/deps/elrond_wasm_output-f7264c09a20c2b42.d
+++
@@ -1,5 +0,0 @@
-/home/shield/Aerovek/rust/dao/wasm/target/debug/deps/elrond_wasm_output-f7264c09a20c2b42.rmeta: /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/elrond-wasm-output-0.34.1/src/lib.rs
-
-/home/shield/Aerovek/rust/dao/wasm/target/debug/deps/elrond_wasm_output-f7264c09a20c2b42.d: /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/elrond-wasm-output-0.34.1/src/lib.rs
-
-/home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/elrond-wasm-output-0.34.1/src/lib.rs:
--- wasm/target/debug/deps/git_version_macro-4a094d78e9e26695.d
+++
@@ -1,6 +0,0 @@
-/home/shield/Aerovek/rust/dao/wasm/target/debug/deps/libgit_version_macro-4a094d78e9e26695.so: /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/lib.rs /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/utils.rs
-
-/home/shield/Aerovek/rust/dao/wasm/target/debug/deps/git_version_macro-4a094d78e9e26695.d: /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/lib.rs /home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/utils.rs
-
-/home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/lib.rs:
-/home/shield/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/git-version-macro-0.3.5/src/utils.rs:
